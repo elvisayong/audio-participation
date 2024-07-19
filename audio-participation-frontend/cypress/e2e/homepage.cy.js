8b52653ae36debf690ba81c060271f1a105f1a8f
elvis <awanchiri@demos-deutschland.de>
2024-07-19 23:30:49 +0200

    Implemented cypress tests and configured eslint for cypress as well as implemented playwright tests

---
+++ audio-participation-frontend/cypress/e2e/homepage.cy.js
@@ -0,0 +1,8 @@
+
+
+describe('Homepage', () => {
+  it('should load the homepage', () => {
+    cy.visit('/');
+    cy.contains('Available Plans'); // Adjust this selector to match something on your homepage
+  });
+});
