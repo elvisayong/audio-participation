55664684dfd8b4bef699dca711b5bf5137ae3eeb
elvis <awanchiri@demos-deutschland.de>
2024-07-20 02:44:41 +0200

    sixth one

--- audio-participation-frontend/cypress/e2e/homepage.cy.js
+++ audio-participation-frontend/cypress/e2e/homepage.cy.js
@@ -3,6 +3,6 @@
 describe('Homepage', () => {
   it('should load the homepage', () => {
     cy.visit('/');
-    cy.contains('Available Plans'); // Adjust this selector to match something on your homepage
+    cy.contains('Available Plans'); // Adjust this selector to match something on your 
   });
 });
